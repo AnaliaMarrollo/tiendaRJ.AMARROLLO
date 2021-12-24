462bdd70ba0e926eefb4eb048c625ebae0b5b53d
anitamarrollo <anam85_1@hotmail.com>
2021-12-24 15:20:00 -0300

    Ajuste en SignUp usuario creado redirecciona a Home

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { useState } from "react";
+import { Redirect } from "react-router";
 
 //CONTEXT:
 import { AuthContext } from "../../context/AuthContext";
@@ -10,7 +11,7 @@ import "../../_custom.scss";
 import "../ContactPage/ContactPage.scss";
 
 const SignUp = () => {
-  const { signup, error } = useContext(AuthContext);
+  const { signup, logged, error } = useContext(AuthContext);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -21,47 +22,53 @@ const SignUp = () => {
   };
 
   return (
-    <div className="white-container-full">
-      <form className="form" onSubmit={handleSignup}>
-        <h5 className="title">Registrarme</h5>
-        <span className="form-span">Email</span>
-        <input
-          type="text"
-          className="form-input"
-          id="email-signup"
-          name="email-singup"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {error === "auth/email-already-in-use" && (
-          <p className="form-message form-message-error">
-            Usuario ya registrado
-          </p>
+  <div className="white-container-full">
+      {logged ?
+        (<Redirect to={"/"} />)
+        : (
+    
+          <form className="form" onSubmit={handleSignup}>
+            <h5 className="title">Registrarme</h5>
+            <span className="form-span">Email</span>
+            <input
+              type="text"
+              className="form-input"
+              id="email-signup"
+              name="email-singup"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {error === "auth/email-already-in-use" && (
+              <p className="form-message form-message-error">
+                Usuario ya registrado
+              </p>
+            )}
+            {error === "auth/invalid-email" && (
+              <p className="form-message form-message-error">
+                Ingrese un email válido
+              </p>
+            )}
+            <span className="form-span">Contaseña</span>
+            <input
+              type="password"
+              className="form-input"
+              id="password-signup"
+              name="password-singup"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error === "auth/weak-password" && (
+              <p className="form-message form-message-error">
+                La contraseña debe contener más de 6 caracteres
+              </p>
+            )}
+            <input
+              type="submit"
+              value="Enviar"
+              className="btn-login btn-container-violet"
+            />
+          </form>
         )}
-        {error === "auth/invalid-email" && (
-          <p className="form-message form-message-error">
-            Ingrese un email válido
-          </p>
-        )}
-        <span className="form-span">Contaseña</span>
-        <input
-          type="password"
-          className="form-input"
-          id="password-signup"
-          name="password-singup"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error === "auth/weak-password" && (
-          <p className="form-message form-message-error">
-            La contraseña debe contener más de 6 caracteres
-          </p>
-        )}
-        <input
-          type="submit"
-          value="Enviar"
-          className="btn-login btn-container-violet"
-        />
-      </form>
     </div>
+    
   );
 };
 
